924022c06aff64bb1cf3b3ad8be64fd07f98c5ec
berniel <383176861@qq.com>
2019-04-06 11:51:34 +0800

    Site updated: 2019-04-06 11:51:33

--- js/src/ban.js
+++ js/src/ban.js
@@ -69,47 +69,58 @@ class currentCirle extends Circle {
   }
 }
 //更新页面用requestAnimationFrame替代setTimeout
-window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+function goPAGE() {
+  if ((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))) {
+      /*window.location.href="你的手机版地址";*/
+      alert("mobile")
+  }
+  else {
+      /*window.location.href="你的电脑版地址";    */
+      window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
 
-let canvas = document.getElementById('canvas');
-let ctx = canvas.getContext('2d');
-let w = canvas.width = window.innerWidth;
-let h = canvas.height = window.innerHeight;
-let circles = [];
-let current_circle = new currentCirle(0, 0)
+      let canvas = document.getElementById('canvas');
+      let ctx = canvas.getContext('2d');
+      let w = canvas.width = window.innerWidth;
+      let h = canvas.height = window.innerHeight;
+      let circles = [];
+      let current_circle = new currentCirle(0, 0)
 
-let draw = function () {
-  ctx.clearRect(0, 0, w, h);
-  for (let i = 0; i < circles.length; i++) {
-      circles[i].move(w, h);
-      circles[i].drawCircle(ctx);
-      for (j = i + 1; j < circles.length; j++) {
-          circles[i].drawLine(ctx, circles[j])
+      let draw = function () {
+        ctx.clearRect(0, 0, w, h);
+        for (let i = 0; i < circles.length; i++) {
+            circles[i].move(w, h);
+            circles[i].drawCircle(ctx);
+            for (j = i + 1; j < circles.length; j++) {
+                circles[i].drawLine(ctx, circles[j])
+            }
+        }
+        if (current_circle.x) {
+            current_circle.drawCircle(ctx);
+            for (var k = 1; k < circles.length; k++) {
+                current_circle.drawLine(ctx, circles[k])
+            }
+        }
+        requestAnimationFrame(draw)
       }
-  }
-  if (current_circle.x) {
-      current_circle.drawCircle(ctx);
-      for (var k = 1; k < circles.length; k++) {
-          current_circle.drawLine(ctx, circles[k])
+
+      let init = function (num) {
+        for (var i = 0; i < num; i++) {
+            circles.push(new Circle(Math.random() * w, Math.random() * h));
+        }
+        draw();
       }
-  }
-  requestAnimationFrame(draw)
-}
+      window.addEventListener('load', init(60));
+      window.onmousemove = function (e) {
+        e = e || window.event;
+        current_circle.x = e.clientX;
+        current_circle.y = e.clientY;
+      }
+      window.onmouseout = function () {
+        current_circle.x = null;
+        current_circle.y = null;
 
-let init = function (num) {
-  for (var i = 0; i < num; i++) {
-      circles.push(new Circle(Math.random() * w, Math.random() * h));
-  }
-  draw();
-}
-window.addEventListener('load', init(60));
-window.onmousemove = function (e) {
-  e = e || window.event;
-  current_circle.x = e.clientX;
-  current_circle.y = e.clientY;
-}
-window.onmouseout = function () {
-  current_circle.x = null;
-  current_circle.y = null;
+      };
 
-};
+  }
+}
+goPAGE();
